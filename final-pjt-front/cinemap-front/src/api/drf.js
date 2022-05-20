b92dce679d41021726cb2b351a4b0070d13645ac
ebyoung <ebyoung5183@naver.com>
2022-05-20 15:00:21 +0900

    Modify api url

--- final-pjt-front/cinemap-front/src/api/drf.js
+++ final-pjt-front/cinemap-front/src/api/drf.js
@@ -1,4 +1,4 @@
-const HOST = 'http://localhost:8000/api/v1/'
+const HOST = 'http://localhost:8000/'
 
 const ACCOUNTS = 'accounts/'
 const REVIEWS = 'reviews/'
@@ -9,6 +9,7 @@ export default {
     login: () => HOST + ACCOUNTS + 'login/',
     logout: () => HOST + ACCOUNTS + 'logout/',
     signup: () => HOST + ACCOUNTS + 'signup/',
+    follow: (username) => HOST + ACCOUNTS + 'follow/' + username + '/',
     // Token 으로 현재 user 판단
     currentUserInfo: () => HOST + ACCOUNTS + 'user/',
     // username으로 프로필 제공
@@ -20,7 +21,7 @@ export default {
     reviews: () => HOST + REVIEWS,
     // /reviews/1/
     review: reviewPk => HOST + REVIEWS + `${reviewPk}/`,
-    likereview: reviewPk => HOST + REVIEWS + `${reviewPk}/` + 'like/',
+    likeReview: reviewPk => HOST + REVIEWS + `${reviewPk}/` + 'like/',
     comments: reviewPk => HOST + REVIEWS + `${reviewPk}/` + COMMENTS,
     comment: (reviewPk, commentPk) =>
       HOST + REVIEWS + `${reviewPk}/` + COMMENTS + `${commentPk}/`,
