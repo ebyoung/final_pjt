371f355f5ba9247cfcb83cf4c93e9da6fc13926a
ebyoung <ebyoung5183@naver.com>
2022-05-25 18:21:08 +0900

    for merge

--- final-pjt-front/cinemap-front/src/App.vue
+++ final-pjt-front/cinemap-front/src/App.vue
@@ -38,7 +38,7 @@
 <style>
 #app {
   background: linear-gradient(#b093e573, rgb(252, 134, 252));
-  background: linear-gradient(70deg, blue, pink);
+  /* background: linear-gradient(70deg, blue, pink); */
   /* padding: 15px 25px 65px; */
 
 }
